5ae5fc104ed7b44197e5b5e76a253c684a04c034
ramesh-suryaneni <ramesh.suryaneni@gmail.com>
2024-02-15 22:20:45 +0530

    initial working

---
+++ database/schema.sql
@@ -0,0 +1,22 @@
+GRANT ALL PRIVILEGES ON *.* TO 'user';
+CREATE DATABASE IF NOT EXISTS mydb;
+USE mydb;
+
+CREATE TABLE IF NOT EXISTS transactions (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id VARCHAR(255),
+  amount VARCHAR(255),
+  currency VARCHAR(255),
+  type VARCHAR(255),
+  country VARCHAR(255),
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS user_profile (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  surname VARCHAR(255),
+  middle_name VARCHAR(255),
+  suspicious_activity VARCHAR(255),
+  PRIMARY KEY (id)
+);  
